5e1a517875d96148d24871b74b97741941c1c81e
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-02-01 14:52:08 -0300

    Feat Inicia tela home com especies

--- src/main.js
+++ src/main.js
@@ -23,34 +23,7 @@ const vuetify = createVuetify({
 const app = createApp(App)
 
 app.use(router).use(vuetify)
-/*
-.use(VuetifyToast, {
-	x: 'right', // default
-	y: 'bottom', // default
-	color: 'info', // default
-	icon: 'info',
-	iconColor: '', // default
-	classes: [
-		'body-2'
-	],
-	timeout: 3000, // default
-	dismissable: true, // default
-	multiLine: false, // default
-	vertical: false, // default
-	queueable: false, // default
-	showClose: false, // default
-	closeText: '', // default
-	closeIcon: 'close', // default
-	closeColor: '', // default
-	slot: [], //default
-	shorts: {
-		custom: {
-			color: 'purple'
-		}
-	},
-	property: '$toast' // default
-})
-*/
+
 
 app.mount('#app')
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,43 @@
 <template>
-  <div>
-    
+  <h1 class="title">Espécies cadastradas</h1>
+  <div class="list-species">
+    <v-card v-for="specie in species" :key="specie.id" width="30%" :title="specie.name" link />
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      species: []
+    }
+  },
+  mounted() {
+    axios
+      .get('http://127.0.0.1:8000/api/species')
+      .then((response) => {
+        this.species = response.data
+      })
+      .catch((error) => {
+        alert('Houve um erro')
+      })
+      .finally(() => {
+        // ver exemplo
+      })
+  }
+}
+</script>
+<style scoped>
+ .title {
+    margin-top: 20px;
+    margin-left: 20px;
+ }
+ .list-species {
+    display: flex;
+    gap: 10px;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    padding: 20px;
+ }
+</style>
