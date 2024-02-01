1493f24312bbf829a15be7c97dde366adc43adf3
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-02-01 14:58:26 -0300

    Feat: add menu

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-  <RouterView />
+  <v-layout>
+    <v-navigation-drawer :rail="rail" permanent>
+      <v-list>
+        <v-list-item
+          prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+          title="Douglas Cavalcante"
+          subtitle="doug@gmail.com"
+        ></v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list density="compact" nav>
+        <v-list-item prepend-icon="mdi-folder" title="Pets" value="myfiles"></v-list-item>
+        <v-list-item prepend-icon="mdi-star" title="Sair" value="starred"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+       <router-view></router-view>
+    </v-main>
+  </v-layout>
 </template>
