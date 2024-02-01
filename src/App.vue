a30984b8ffe3dbbd10fde02fe17911552ca0749a
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-02-01 13:06:09 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+  <RouterView />
+</template>
