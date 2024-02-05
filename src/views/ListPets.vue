c44c81d2af16e6548542929e2a11c9a7ae25c24b
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-02-05 12:19:30 -0300

    Adiciona tradução para o tamanho do pet

--- src/views/ListPets.vue
+++ src/views/ListPets.vue
@@ -1,71 +1,78 @@
 <template>
     <v-conteiner>
         <h1>Lista de Pets</h1>
-    <v-table>
-      <thead class="header-table">
-        <tr>
-          <th class="text-left">
-            Nome
-          </th>
-          <th class="text-left">
-            Raça
-          </th>
-          <th class="text-left">
-            Porte
-          </th>
-          <th class="text-left">
-            Peso(Kg)
-          </th>
-          <th class="text-left">
-            Idade
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="pet in pets"
-          :key="pet.id"
-        >
-          <td>{{ pet.pet_name }}</td>
-          <td>{{ pet.race.name }}</td>
-          <td>{{ pet.size }}</td>
-          <td>{{ pet.weight }}</td>
-          <td>{{ pet.age}}</td>
-         
-        </tr>
-      </tbody>
-    </v-table>
-</v-conteiner>
-  </template>
+        <v-table>
+            <thead class="header-table">
+                <tr>
+                    <th class="text-left">Nome</th>
+                    <th class="text-left"> Raça</th>
+                    <th class="text-left">Porte</th>
+                    <th class="text-left">Peso(Kg)</th>
+                    <th class="text-left">Idade</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="pet in pets" :key="pet.id">
+                    <td>{{ pet.pet_name }}</td>
+                    <td>{{ pet.race.name }}</td>
+                    <td>{{ this.translateSize(pet.size) }}</td>
+                    <td>{{ pet.weight }}</td>
+                    <td>{{ pet.age }}</td>
+
+                </tr>
+            </tbody>
+        </v-table>
+    </v-conteiner>
+</template>
 <script>
 import PetService from '../services/PetService'
-export default{
+export default {
 
-    data(){
-        return{
+    data() {
+        return {
             pets: []
         }
     },
-    mounted(){
-        PetService.getAllPets(this.$route.params.id)
-        .then((data)=>{
-            this.pets = data
-        })
-        .catch(()=>{
-            alert("Houve um erro ")
-        })
+    methods:{
+        translateSize(name){
+            switch(name){
+            case'SMALL':{
+                return 'Pequeno'
+            }
+            case'MEDIUM':{
+                return 'Médio'
+            }
+            case'LARGE':{
+                return 'Grande'
+            }
+            case'EXTRA_LARGE':{
+                return 'Gigante'
+            }
+            
 
-    }}
-    
+        }}
+    },
+    mounted() {
+        PetService.getAllPets(this.$route.params.id)
+            .then((data) => {
+                this.pets = data
+            })
+            .catch(() => {
+                alert("Houve um erro ")
+            })
+
+    }
+}
+
 </script>
 <style scoped>
-.header-table{
+.header-table {
     background-color: tomato;
     color: #fff;
-    font-weight:bold ;
-}
-tbody tr:nth-child(2n){
- background: #e9e7e7;
+    font-weight: bold;
 }
 
+tbody tr:nth-child(2n) {
+    background: #e9e7e7;
+}
 </style>
