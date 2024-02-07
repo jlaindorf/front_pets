df145d038c5c47e421d1450be5c88c2df78c9c46
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-02-07 12:54:39 -0300

    adiciona input de especie

--- src/views/FormPet.vue
+++ src/views/FormPet.vue
@@ -13,19 +13,31 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col cols="12">
+      <v-col cols="6">
         <v-select
             label="Tamanho"
             :items="itemsSize"
             variant="outlined"
             placeholder="Selecione um item"
+            v-model="size"
+          />
+      </v-col>
+      <v-col cols="6">
+        <v-select
+            label="Espécie"
+            :items="itemsSpecies"
+            item-title="name"
+            item-value="id"
+            variant="outlined"
+            v-model="specie"
           />
       </v-col>
     </v-row>
   </v-card>
 </template>
 <script>
-import optionsSize from '../constants/pet.constants'
+import {optionsSize} from '../constants/pet.constants'
+import SpecieService from '../services/SpecieService';
 
 export default {
   data() {
@@ -35,8 +47,20 @@ export default {
       age: 1,
       weight: 1,
       itemsSize:  optionsSize ,
+      size:'',
+      itemsSpecies :[],
+      specie:''
     }
-  }
+  },
+  mounted(){
+    SpecieService.getAllSpecies()
+    .then(data =>{
+      this.itemsSpecies = data
+    })
+    .catch(() => {
+        alert('Houve um erro')
+  })
+}
 }
 </script>
 <style></style>
