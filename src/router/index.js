e59acfb0aee0bc6580fde6de664467d1e8d5d255
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-02-13 18:18:30 -0300

    Cria const para salvar rotas

--- src/router/index.js
+++ src/router/index.js
@@ -6,40 +6,41 @@ import Login from '../views/Login.vue'
 import ListProfessionals from '../views/ListProfessionals.vue'
 import Aprendizado from '../views/Aprendizado.vue'
 
+const routes = [
+  {
+    path: '/',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/pets/:id',
+    name: 'ListPets',
+    component: ListPets
+  },
+  {
+    path: '/pets/novo',
+    name: 'FormPet',
+    component: FormPet
+  },
+  {
+    path: '/veterinarios',
+    name: 'ListProfessionals',
+    component: ListProfessionals
+  },
+  {
+    path: '/aprendizado',
+    name: 'Aprendizado',
+    component: Aprendizado
+  },
+]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'login',
-      component: Login
-    },
-    {
-      path: '/home',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/pets/:id',
-      name: 'ListPets',
-      component: ListPets
-    },
-    {
-      path: '/pets/novo',
-      name: 'FormPet',
-      component: FormPet
-    },
-    {
-      path: '/veterinarios',
-      name: 'ListProfessionals',
-      component: ListProfessionals
-    },
-    {
-      path: '/aprendizado',
-      name: 'Aprendizado',
-      component: Aprendizado
-    },
-  ]
+  routes: routes
 })
 
 export default router
