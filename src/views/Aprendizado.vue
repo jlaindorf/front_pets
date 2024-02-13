9ae27513f0cfe75a5169c73d58ea1dfb1278062a
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-02-13 18:34:31 -0300

    Adiciona inputs de tafera

--- src/views/Aprendizado.vue
+++ src/views/Aprendizado.vue
@@ -1,5 +1,36 @@
 <template>
-    <div>
-        
-    </div>
+    <h1>Minhas Tarefas</h1>
+    <form @submit.prevent="handleAddItem">
+        <input type="text" placeholder="Digite sua tarefa" v-model="description">
+        <button type="submit">Cadastrar</button>
+    </form>
+    <ol>
+        <li v-for="item in list" :key="item">
+            {{item}}
+        </li>
+    </ol>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            description: '',
+            list: []
+        }
+    },
+    methods: {
+        handleAddItem() {
+            this.list = [...this.list, this.description]
+            this.description =''
+        }
+    }
+
+}
+</script>
+<style scoped>
+input {
+    border: 1px solid #000;
+    background-color: #ccc;
+    margin:20px;
+}
+</style>
