b3520d14675d658c5b1577accd1a18e65fb08912
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-02-15 13:03:59 -0300

    add.testa se está vindo o array de especies e os cards respectivos

--- src/views/Home.spec.js
+++ src/views/Home.spec.js
@@ -5,6 +5,7 @@ import { flushPromises, mount } from '@vue/test-utils'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import SpecieService from '../services/SpecieService'
 
 const vuetify = createVuetify({
     components,
@@ -13,13 +14,53 @@ const vuetify = createVuetify({
 
 global.ResizeObserver = require('resize-observer-polyfill')
 
-describe("Tela Home",()=>{
-    it("Espera-se que a tela seja renderizada",()=>{
+describe("Tela Home", () => {
+    vi.spyOn(SpecieService, 'getAllSpecies').mockResolvedValue([
+        {
+            id: 1,
+            name: 'cachorro'
+        },
+        {
+            id: 2,
+            name: 'gato'
+        },
+        {
+            id: 3,
+            name: 'pássaro'
+        },
+        {
+            id: 3,
+            name: 'peixe'
+        }
+    ])
+    it("Espera-se que a tela seja renderizada", () => {
         const component = mount(Home, {
             global: {
                 plugins: [vuetify]
             }
         })
         expect(component).toBeTruthy()
+    })
+    it("Espera-se que exiba na tela os nomes das espécies",async () => {
+        const component = mount(Home, {
+            global: {
+                plugins: [vuetify]
+            }
+        })
+        await flushPromises()
+        expect(component.text()).toContain("cachorro")
+        expect(component.text()).toContain("pássaro")
+        expect(component.text()).toContain("gato")
+        expect(component.text()).toContain("peixe")
+    })
+    it("Espera-se que exiba na tela os cards para cada espécie",async () => {
+        const component = mount(Home, {
+            global: {
+                plugins: [vuetify]
+            }
+        })
+        await flushPromises()
+        const cards =  component.findAll("[data-test ='card-item']")
+        expect(cards).toHaveLength(4)
     })
 })
