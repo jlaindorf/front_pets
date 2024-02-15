0c70e6aa7441559345b0a5677baddacb3e8f99c5
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-02-15 11:55:25 -0300

    Testa then e catch tela de login

--- src/views/Login.spec.js
+++ src/views/Login.spec.js
@@ -18,34 +18,64 @@ const vuetify = createVuetify({
 
 global.ResizeObserver = require('resize-observer-polyfill')
 
-describe('Tela de login',()=>{
-    it('Espera-se que a tela seja renderizada',()=>{
-        vi.spyOn(AuthenticationService,'login').mockResolvedValue({
+describe('Tela de login', () => {
+    it('Espera-se que a tela seja renderizada', () => {
+        vi.spyOn(AuthenticationService, 'login').mockResolvedValue({
             data: {
                 token: 'token',
                 permissions: []
             }
         })
 
-      const component =  mount(Login,{
-        global:{
-            plugins:[vuetify]
-        }
-      })
-      expect(component).toBeTruthy()
+        const component = mount(Login, {
+            global: {
+                plugins: [vuetify]
+            }
+        })
+        expect(component).toBeTruthy()
     })
 
-    it('Espera-se que ao submeter o formulário, seja redirecional para a tela home',()=>{
-        const component =  mount(Login,{
-            global:{
-                plugins:[vuetify]
+    it('Espera-se que ao submeter o formulário, seja redirecional para a tela home', () => {
+        const login = vi.spyOn(AuthenticationService, 'login').mockResolvedValue({
+            data: {
+                token: 'token',
+                permissions: []
             }
-          })
-          //getComponent quando for um componente externo
-          component.getComponent("[data-test ='input-email']").setValue("j@gmail.com")
-          component.getComponent("[data-test ='input-password']").setValue("12345678")
+        })
 
-          component.getComponent("[data-test ='submit-button']").trigger("submit")
-       
+        const component = mount(Login, {
+            global: {
+                plugins: [vuetify]
+            }
+        })
+        //getComponent quando for um componente externo
+        component.getComponent("[data-test ='input-email']").setValue("j@gmail.com")
+        component.getComponent("[data-test ='input-password']").setValue("12345678")
+
+        component.getComponent("[data-test ='submit-button']").trigger("submit")
+
+        expect(login).toBeCalledTimes(1) //testa se o login foi chamdo 1vez ao clicar no submit
+        expect(login).toBeCalledWith({ email: 'j@gmail.com', password: '12345678' })
+
+    })
+   it('Espera-se que ao submeter o formulário, receba uma mensagem de erro', async () => {
+        vi.spyOn(AuthenticationService, 'login').mockRejectedValue(new Error())
+
+        const component = mount(Login, {
+            global: {
+                plugins: [vuetify]
+            }
+        })
+        //getComponent quando for um componente externo
+        component.getComponent("[data-test ='input-email']").setValue("j@gmail.com")
+        component.getComponent("[data-test ='input-password']").setValue("12345678")
+
+        component.getComponent("[data-test ='submit-button']").trigger("submit")
+
+        await flushPromises()
+
+        expect(component.text()).toContain("Houve um erro ao realizar o login")
+
+
     })
 })
